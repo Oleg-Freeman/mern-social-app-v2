1ee26b7091f53883d5464fdfa23bd539bcf11127
Oleg Freeman <av.ssf4@gmail.com>
2024-03-02 10:38:59 +0200

    Fix: dependabot alerts

--- db/index.js
+++ db/index.js
@@ -1,10 +1,7 @@
 const mongoose = require('mongoose');
 
 function connect(mongoUrl) {
-    mongoose.connect(mongoUrl, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    });
+    mongoose.connect(mongoUrl);
     mongoose.connection.once('open', () => {
         console.log('MongoDB database connection established successfully');
     });
